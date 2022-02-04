682afe5c6a3bc16d5136f4fd4be36a466c779cb5
Oshadi Godage <66914300+oshgodage@users.noreply.github.com>
2022-02-04 23:49:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var selectedRow = null
 
 function onFormSubmit(e) {
-		event.preventDefault();
+	event.preventDefault();
         var formData = readFormData();
         if (selectedRow == null){
             insertNewRecord(formData);
