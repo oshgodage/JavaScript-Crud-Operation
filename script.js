8e3f51c95aa0b9f097d1cb18af8702c83c5810c7
Oshadi Godage <66914300+oshgodage@users.noreply.github.com>
2022-02-04 23:49:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-var selectedRow = null;
-function onFormSubmit(e){
-    event.preventDefault();
-    var formData = readFormData();
-    if(selectedRow === null){
-        insertNewRecord(formData);
-    }
-    else{
-        updateRecord(formData);
-    }
-    resetForm();
+var selectedRow = null
+
+function onFormSubmit(e) {
+		event.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
 }
 
 //Retrieve the data
-function readFormData(){
+function readFormData() {
     var formData = {};
     formData["productCode"] = document.getElementById("productCode").value;
     formData["product"] = document.getElementById("product").value;
@@ -22,31 +23,30 @@ function readFormData(){
 }
 
 //Insert the data
-function insertNewRecord(data){
+function insertNewRecord(data) {
     var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
-    var cell1 = newRow.insertCell(0);
-        cell1.innerHTML = data.productCode;
-    var cell2 = newRow.insertCell(1);
-        cell2.innerHTML = data.product;
-    var cell3 = newRow.insertCell(2);
-        cell3.innerHTML = data.qty;
-    var cell4 = newRow.insertCell(3);
-        cell4.innerHTML = data.perPrice;
-    var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.productCode;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.product;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.qty;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.perPrice;
+    cell4 = newRow.insertCell(4);
+        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
 }
 
 //Edit the data
-function onEdit(td){
+function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById('productCode').value = selectedRow.cells[0].innerHTML;
-    document.getElementById('product').value = selectedRow.cells[1].innerHTML;
-    document.getElementById('qty').value = selectedRow.cells[2].innerHTML;
-    document.getElementById('perPrice').value = selectedRow.cells[3].innerHTML;
+    document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
 }
-
-function updateRecord(formData){
+function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.productCode;
     selectedRow.cells[1].innerHTML = formData.product;
     selectedRow.cells[2].innerHTML = formData.qty;
@@ -54,18 +54,19 @@ function updateRecord(formData){
 }
 
 //Delete the data
-function onDelete(td){
-    if(confirm('Do you want to delete this record?')){
+function onDelete(td) {
+    if (confirm('Do you want to delete this record?')) {
         row = td.parentElement.parentElement;
         document.getElementById('storeList').deleteRow(row.rowIndex);
+        resetForm();
     }
-    resetForm();
 }
 
 //Reset the data
-function resetForm(){
-    document.getElementById('productCode').value = '';
-    document.getElementById('product').value = '';
-    document.getElementById('qty').value = '';
-    document.getElementById('perPrice').value = '';
+function resetForm() {
+    document.getElementById("productCode").value = '';
+    document.getElementById("product").value = '';
+    document.getElementById("qty").value = '';
+    document.getElementById("perPrice").value = '';
+    selectedRow = null;
 }
